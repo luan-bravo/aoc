7f05b61deeb2e00b02ba7ff459459039c2ad5c46
luan-brav0 <luan-brav0@github.com>
2025-04-21 03:19:28 -0300

    TODO: write file handler to import in other days

---
+++ file_handler/file_handler.h
@@ -0,0 +1,14 @@
+#ifndef FILE_HANDLER_H
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX_FILE_SIZE
+#define MAX_FILE_SIZE
+#define MAX_FILE_SIjjjjZE
+
+typedef struct {
+    FILE* file;
+    size_t cursor;
+} file_t;
+
+#endif
